bc71e7805e3eccaf9ded2bd4e42f068cc7f17268
asad-baig <baiga4@gmail.com>
2020-07-16 14:25:39 -0400

    Using FlatMap in Streams

--- src/main/java/java8exercises/imperative/FlatCollection.java
+++ src/main/java/java8exercises/imperative/FlatCollection.java
@@ -2,6 +2,7 @@ package java8exercises.imperative;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class FlatCollection {
 
@@ -9,13 +10,9 @@ public class FlatCollection {
     }
 
     public static List<String> transform(List<List<String>> collection) {
-        List<String> newCollection = new ArrayList<>();
-        for (List<String> subCollection : collection) {
-            for (String value : subCollection) {
-                newCollection.add(value);
-            }
-        }
-        return newCollection;
+        return collection.stream()
+               .flatMap(item -> item.stream())
+               .collect(Collectors.toList());
     }
 
 

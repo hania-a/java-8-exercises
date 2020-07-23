386d9c34808385cd1f20aa35ee020571077304b6
Srivastava, Mehul <msrivastava@netsuite.com>
2020-07-23 13:44:00 -0400

    modified Kids.java

--- src/main/java/java8exercises/imperative/Kids.java
+++ src/main/java/java8exercises/imperative/Kids.java
@@ -5,19 +5,15 @@ import java8exercises.Person;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Kids {
 
     public static Set<String> getKidNames(List<Person> people) {
-        Set<String> kids = new HashSet<>();
-        for (Person person : people) {
-            if (person.getAge() < 18) {
-                kids.add(person.getName());
-            }
-        }
-        return kids;
+        return people.stream()
+                .filter(person -> person.getAge() < 18)
+                .map(Person::getName)
+                .collect(Collectors.toSet());
     }
-
-
 
 }

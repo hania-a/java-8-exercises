2893f927c50ffa4d4b3bbb90d6f240ee9230ae5f
mukhan <mushahidnawaz@hotmail.com>
2020-07-23 14:06:42 -0400

    Did the olderpersonspec class

--- src/test/java/java8exercises/imperative/OldestPersonSpec.java
+++ src/test/java/java8exercises/imperative/OldestPersonSpec.java
@@ -3,6 +3,7 @@ package java8exercises.imperative;
 import java8exercises.Person;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static java.util.Arrays.asList;
@@ -17,7 +18,13 @@ public class OldestPersonSpec {
         Person viktor = new Person("Viktor", 40);
         Person eva = new Person("Eva", 42);
         List<Person> collection = asList(sara, eva, viktor);
-        assertThat(getOldestPerson(collection)).isEqualToComparingFieldByField(eva);
+        assertThat(OldestPerson.getOldestPerson(collection)).isEqualToComparingFieldByField(eva);
     }
 
+    @Test
+    public void getOldestPersonShouldReturnNull() {
+        List<Person> collection = new ArrayList<>();
+        Person oldestPerson = getOldestPerson(collection);
+        assert oldestPerson == null;
+    }
 }

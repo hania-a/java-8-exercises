87460a9022b3816c58587077498ea8793df16b8e
knagraj <knagraj@netsuite.com>
2020-08-06 14:29:35 -0400

    Added method2 for Reduce

---
+++ src/main/java/java8exercises/reduce/ReduceExercise1.java
@@ -0,0 +1,42 @@
+package java8exercises.reduce;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+public class ReduceExercise1 {
+
+    public static void main(String[] args) {
+        method1();
+
+        method2();
+    }
+
+    /**
+     * Produce a single String that is the result of concatenating the uppercase versions of all of the
+     * Strings. E.g., the result should be "HIHELLO...". Use a single reduce operation, without using map.
+     * */
+    private static void method1() {
+        List<String> words = Arrays.asList("hi", "hello", "asad", "kaustubh", "danish", "B2", "ms");
+        Optional<String> outputVal;
+
+        outputVal = words.stream()
+                .reduce((a, b) -> a.concat(b).toUpperCase());
+        System.out.println(outputVal);
+    }
+
+    /**
+     * Produce the same String as above, but this time via a map operation that turns the words into upper
+     * case, followed by a reduce operation that concatenates them.
+     */
+    private static void method2() {
+        List<String> words = Arrays.asList("hi", "hello", "asad", "kaustubh", "danish", "B2", "ms");
+
+        Optional<String> reduce = words.stream()
+                .map(String::toUpperCase)
+                .reduce(String::concat);
+
+        System.out.println("Method2: " + reduce.get());
+    }
+
+}
